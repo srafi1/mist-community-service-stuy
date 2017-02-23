6f49d04f2c3d8f515be44daa59a3853dcb8ad989
srafi1 <shakilrafi0@gmail.com>
2017-02-23 16:05:16 -0500

    Automatic commit message

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('404.html'), 404
+
 @app.route('/')
+@app.route('/home')
 def home():
-    return 'If you can read this, then the site didn\'t crash :D'
+    return render_template('index.html', page='home')
+
+@app.route('/opportunities')
+def opportunities():
+    return render_template('opportunities.html', page='opportunities')
+
+@app.route('/studyhelp')
+def studyhelp():
+    return render_template('studyhelp.html', page='studyhelp')
+
+@app.route('/testprep')
+def testprep():
+    return render_template('testprep.html', page='testprep')
+
+@app.route('/studytips')
+def studytips():
+    return render_template('studytips.html', page='studytips')
 
 if '__main__' == __name__:
     app.run(debug=True)
